5f5bc2d8952b9c9a00b84bea40ba43cec597a1bb
zhongbin <zhongbin.wen@akuvox.com>
2025-01-06 14:20:24 +0800

    MMM

--- AKTalkSDK/AKTalkSDK.framework/Headers/AKTalkSDK.h
+++ AKTalkSDK/AKTalkSDK.framework/Headers/AKTalkSDK.h
@@ -2,8 +2,7 @@
 //  AKTalkSDK.h
 //  AKTalkSDK
 //
-//  Created by 许志明 on 2020/2/6.
-//  Copyright © 2020 akuvox. All rights reserved.
+//  Created by mac_mini2 on 2025/1/3.
 //
 
 #import <Foundation/Foundation.h>
@@ -15,7 +14,7 @@ FOUNDATION_EXPORT double AKTalkSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char AKTalkSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AKTalkSDK/PublicHeader.h>
-#import <AKTalkSDK/AKTalkManager.h>
+
 #import <AKTalkSDK/AKSIPModel.h>
 #import <AKTalkSDK/AKIncomingModel.h>
-
+#import <AKTalkSDK/AKTalkManager.h>
